2ec5fce2409097a99177ca3278832ace17c501ee
AwesomeCodingGuy <sonla@gmx.net>
2020-09-14 19:58:14 +0200

    Shortened main function.

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,21 @@
 #include <fstream>
 #include <sstream>
 #include <iomanip>
+#include <map>
 
 #include "ascii85.h"
 #include "bitwise.h"
 #include "network.h"
+
+
+static std::map<int, std::string> layerMap = {
+    {0, "data/layer0_payload.txt"},
+    {1, "data/layer1_payload.txt"},
+    {2, "data/layer2_payload.txt"},
+    {3, "data/layer3_payload.txt"},
+    {4, "data/layer4_payload.txt"},
+    {5, "data/layer5_payload.txt"},
+};
 
 std::string readPayloadFromFile(std::string fileName)
 {
@@ -19,83 +30,51 @@ std::string readPayloadFromFile(std::string fileName)
     return strm.str();
 }
 
+std::vector<uint8_t> getEncodedLayer(int layer)
+{
+    auto it = layerMap.find(layer);
+    if(it != layerMap.end()) {
+        // get filename to layer
+        std::string fileName = it->second;
+        // get encoded payload from file
+        std::string encoded_payload = readPayloadFromFile(fileName);
+        // decode the payload
+        return ascii85_decode(std::vector<uint8_t>(encoded_payload.begin(),
+                                                   encoded_payload.end()));
+    } else {
+        return std::vector<uint8_t>();
+    }
+}
+
 int main(int argc, char **argv)
 {
-    int layer = 4;
+    const int layer = 4;
+
+    // Get encoded payload from file
+    auto encoded_payload = getEncodedLayer(layer);
+
+    std::vector<uint8_t> encoded_data;
 
     if(layer == 0) {
-        // Get encoded payload from file
-        std::string encoded_layer0 = readPayloadFromFile("data/layer0_payload.txt");
-
-        // decode layer 0 string
-        std::vector<uint8_t> decoded_layer0 = ascii85_decode(std::vector<uint8_t>(encoded_layer0.begin(),
-                                                                                  encoded_layer0.end()));
-
-        // print decoded layer 0
-        std::cout << std::string(decoded_layer0.begin(), decoded_layer0.end()) << std::endl;
+        encoded_data = encoded_payload;
     } else if(layer == 1) {
-        // Get encoded payload from file
-        std::string encoded_layer0 = readPayloadFromFile("data/layer1_payload.txt");
-
-        // decode layer 0 string
-        std::vector<uint8_t> decoded_layer0 = ascii85_decode(std::vector<uint8_t>(encoded_layer0.begin(),
-                                                                                  encoded_layer0.end()));
-
         // decode layer 1
-        std::vector<uint8_t> decoded_layer1 = bitwise_decode(decoded_layer0);
-
-        // print decoded layer 2
-        std::cout << std::string(decoded_layer1.begin(), decoded_layer1.end()) << std::endl;
+        encoded_data = bitwise_decode(encoded_payload);
     } else if(layer == 2) {
-        // Get encoded payload from file
-        std::string encoded_layer0 = readPayloadFromFile("data/layer2_payload.txt");
-
-        // decode layer 0 string
-        std::vector<uint8_t> decoded_layer0 = ascii85_decode(std::vector<uint8_t>(encoded_layer0.begin(),
-                                                                                  encoded_layer0.end()));
-
         // decode layer 2
-        std::vector<uint8_t> decoded_layer2 = parity_decode(decoded_layer0);
-
-        // print decoded layer 3
-        std::cout << std::string(decoded_layer2.begin(), decoded_layer2.end()) << std::endl;
+        encoded_data = parity_decode(encoded_payload);
     } else if(layer == 3) {
-        // Get encoded payload from file
-        std::string encoded_layer0 = readPayloadFromFile("data/layer3_payload.txt");
-
-        // decode layer 0 string
-        std::vector<uint8_t> decoded_layer0 = ascii85_decode(std::vector<uint8_t>(encoded_layer0.begin(),
-                                                                                  encoded_layer0.end()));
         // decode layer 3
-        std::vector<uint8_t> decoded_laye3 = xor_decode(decoded_layer0);
-
-        // print decoded layer 3
-        std::cout << std::string(decoded_laye3.begin(), decoded_laye3.end()) << std::endl;
+        encoded_data = xor_decode(encoded_payload);
     } else if(layer == 4) {
-        // Get encoded payload from file
-        std::string encoded_layer0 = readPayloadFromFile("data/layer4_payload.txt");
-
-        // decode layer 0 string
-        std::vector<uint8_t> decoded_layer0 = ascii85_decode(std::vector<uint8_t>(encoded_layer0.begin(),
-                                                                                  encoded_layer0.end()));
-        // decode layer 3
-        std::vector<uint8_t> decoded_laye3 = network_decode(decoded_layer0);
-
-        // print decoded layer 3
-        std::cout << std::string(decoded_laye3.begin(), decoded_laye3.end()) << std::endl;
+        // decode layer 4
+        encoded_data = network_decode(encoded_payload);
     } else if(layer == 5) {
-        // Get encoded payload from file
-        std::string encoded_layer0 = readPayloadFromFile("data/layer4_payload.txt");
-
-        // decode layer 0 string
-        std::vector<uint8_t> decoded_layer0 = ascii85_decode(std::vector<uint8_t>(encoded_layer0.begin(),
-                                                                                  encoded_layer0.end()));
-        // decode layer 3
-        std::vector<uint8_t> decoded_laye3 = network_decode(decoded_layer0);
-
-        // print decoded layer 3
-        std::cout << std::string(decoded_laye3.begin(), decoded_laye3.end()) << std::endl;
+        encoded_data = std::vector<uint8_t>();
     }
+
+    // Print Layer
+    std::cout << std::string(encoded_data.begin(), encoded_data.end()) << std::endl;
 
     return 0;
 }
